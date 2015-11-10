918eca5ea015fcb8a48201220efac2d601a42aa7
Patrick Ohl <istpaohll@MI-2014-2.fbi.h-da.de>
2015-11-10 12:21:30 +0100

    Termin 2 fertig

--- Termin2/Termin2Aufgabe4.c
+++ Termin2/Termin2Aufgabe4.c
@@ -16,12 +16,12 @@ void taste_irq_handler (void)
 	StructAIC* aicbase = AIC_BASE;
 	StructPIO* piobaseb = PIOB_BASE;
 
-	if(piobaseb->PIO_PDSR & 0x008)
+	if(!(piobaseb->PIO_PDSR & KEY1))
 		piobaseb->PIO_CODR = 0x100;
-	if(piobaseb->PIO_PDSR & 010)
+	if(!(piobaseb->PIO_PDSR & KEY2))
 		piobaseb->PIO_SODR = 0x100;
 	
-	aicbase->AIC_EOICR = piobaseb-> PIO_ISR;
+	aicbase->AIC_EOICR = piobaseb-> PIO_ISR;//wert in das end of register schreiben
 
 }
 
@@ -40,8 +40,15 @@ int main(void)
 	aicbase->AIC_ICCR = 1 << 14;  //delete interrupts
 	aicbase->AIC_SVR[PIOB_ID] = (unsigned int) taste_irq_handler;
 	aicbase->AIC_SMR[PIOB_ID] = 0x7;   //Priorität
-	aicbase->AIC_IECR = 1 <<14; //interrupts für PIOB aktivieren
-	piobaseb-> PIO_IER = 0x300;
+	aicbase->AIC_IECR = 1 << 14; //interrupts für PIOB aktivieren
+	piobaseb->PIO_IER = 0x18; //interrupt für Taster Aktivieren
+	
+	while(1){
+	int i;
+	piobaseb->PIO_CODR=0x200;
+	for(i=0;i<250000;i++){}
+	piobaseb->PIO_SODR=0x200;
+	for(i=0;i<250000;i++){}	
 	
 	return 0;
 }
